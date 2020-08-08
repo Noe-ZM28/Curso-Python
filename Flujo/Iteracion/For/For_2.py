309bc3ac92bfe9306fc7bb9074c65a9342a11699
Noe Zuñiga Morales <noe.zuniga@metrics.digital@gmail.com>
2020-08-07 21:16:04 -0500

    agrego los apuntes de la clase del curso de python, for

---
+++ Flujo/Iteracion/For/For_2.py
@@ -0,0 +1,6 @@
+numeros = [0, 1, 2, 3,4, 5, 6, 7, 8, 9]
+#indice = 0
+
+for indice,i in enumerate(numeros):
+    numeros[indice] *= 10
+print(numeros)
---
+++ Flujo/Iteracion/For/For_3.py
@@ -0,0 +1,11 @@
+cadena = "awa de uwu que sabe a owo"
+
+for caracter in cadena:
+    print (caracter)
+
+cadena2 =""
+for c in cadena:
+    cadena2 += caracter * 2
+
+print (cadena2)
+print (cadena)
